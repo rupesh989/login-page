c7370c3112ff463d0c74d1d779b77273fab14c5b
rupesh989 <rupeshbavaskar24@gmail.com>
2024-01-21 15:25:33 +0530

    addet toggle functionality

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-
 console.log("Login page for practice");
+
+const registerBtn = document.getElementById('register');
+
+const container = document.getElementById('container');
+
+const loginBtn =document.getElementById('login');
+
+registerBtn.addEventListener('click', () =>{
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () =>{
+    container.classList.remove("active");
+});
